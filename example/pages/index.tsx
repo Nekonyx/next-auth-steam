2851ff31c1db271e5656799a58d1f101a2de012d
Nekonyx <nekonyx@proton.me>
2023-04-15 15:30:49 +0600

    chore: add example

---
+++ example/pages/index.tsx
@@ -0,0 +1,18 @@
+import { useSession } from 'next-auth/react'
+
+export default function IndexPage() {
+  const session = useSession({
+    required: true
+  })
+
+  if (session.status !== 'authenticated') {
+    return `You're not authenticated`
+  }
+
+  return (
+    <div>
+      <h1>next-auth-steam</h1>
+      <pre>{JSON.stringify(session.data, null, 2)}</pre>
+    </div>
+  )
+}
