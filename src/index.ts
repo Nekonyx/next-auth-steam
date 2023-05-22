426b9da833ef6749402c97cc4b5b6e38e6bbf6a7
Alexe Vlad <Alexe Vlad>
2023-05-22 10:46:58 +0300

    changed multiple things

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Steam } from './steam'
-
-export * from './constants'
-
-export default Steam
--- src/steam.ts
+++ src/steam.ts
@@ -1,113 +1,139 @@
-import { randomUUID } from 'crypto'
-import { RelyingParty } from 'openid'
-import { TokenSet } from 'openid-client'
+import type { NextApiRequest } from "next";
+import type { OAuthConfig, OAuthUserConfig } from "next-auth/providers";
+import type { NextRequest } from "next/server";
 
-import {
-  EMAIL_DOMAIN,
-  PROVIDER_ID,
-  PROVIDER_NAME,
-  SteamProfile
-} from './constants'
+import { randomUUID } from "crypto";
+import { RelyingParty } from "openid";
+import { TokenSet } from "openid-client";
 
-import type { NextApiRequest } from 'next'
-import type { OAuthConfig, OAuthUserConfig } from 'next-auth/providers'
-import type { NextRequest } from 'next/server'
-
-// prettier-ignore
-export interface SteamProviderOptions extends Partial<OAuthUserConfig<SteamProfile>> {
-  /** @example 'https://example.com/api/auth/callback' */
-  callbackUrl: string | URL
+export enum CommunityVisibilityState {
+  Private = 1,
+  Public = 3,
 }
 
-export function Steam(
-  req: NextApiRequest | NextRequest,
-  options: SteamProviderOptions
-): OAuthConfig<SteamProfile> {
-  const callbackUrl = new URL(options.callbackUrl)
+export enum PersonaState {
+  Offline = 0,
+  Online = 1,
+  Busy = 2,
+  Away = 3,
+  Snooze = 4,
+  LookingToTrade = 5,
+  LookingToPlay = 6,
+}
 
-  // https://example.com
-  // https://example.com/api/auth/callback/steam
-  const realm = callbackUrl.origin
-  const returnTo = `${callbackUrl.href}/${PROVIDER_ID}`
+export interface SteamProfile extends Record<string, any> {
+  steamid: string;
+  communityvisibilitystate: CommunityVisibilityState;
+  profilestate: number;
+  personaname: string;
+  profileurl: string;
+  avatar: string;
+  avatarmedium: string;
+  avatarfull: string;
+  avatarhash: string;
+  lastlogoff: number;
+  personastate: PersonaState;
+  primaryclanid: string;
+  timecreated: number;
+  personastateflags: number;
+  commentpermission: boolean;
+}
+
+export default function Steam<P extends SteamProfile>(
+  req: NextApiRequest | NextRequest,
+  options: OAuthUserConfig<P> & {
+    callbackUrl: string | URL;
+  }
+): OAuthConfig<P> {
+  const { callbackUrl, ...rest } = options;
+  const steamCallbackURL = new URL(options.callbackUrl);
+
+  /** @example 'https://example.com/' */
+  const realm = steamCallbackURL.origin;
+  /** @example 'https://example.com/api/auth/callback/steam' */
+  const returnTo = `${steamCallbackURL.href}/steam`;
 
   return {
-    // @ts-expect-error
-    options,
-    id: PROVIDER_ID,
-    name: PROVIDER_NAME,
-    type: 'oauth',
+    id: "steam",
+    name: "Steam",
+    type: "oauth",
     style: {
-      logo: 'https://raw.githubusercontent.com/nekonyx/next-auth-steam/master/logo/steam.svg',
+      logo: "https://raw.githubusercontent.com/nekonyx/next-auth-steam/master/logo/steam.svg",
       logoDark:
-        'https://raw.githubusercontent.com/nekonyx/next-auth-steam/master/logo/steam-dark.svg',
-      bg: '#000',
-      text: '#fff',
-      bgDark: '#000',
-      textDark: '#fff'
+        "https://raw.githubusercontent.com/nekonyx/next-auth-steam/master/logo/steam-dark.svg",
+      bg: "#000",
+      text: "#fff",
+      bgDark: "#000",
+      textDark: "#fff",
     },
     idToken: false,
-    checks: ['none'],
-    clientId: PROVIDER_ID,
+    checks: ["none"],
+    clientId: "steam",
     authorization: {
-      url: 'https://steamcommunity.com/openid/login',
+      url: "https://steamcommunity.com/openid/login",
       params: {
-        'openid.mode': 'checkid_setup',
-        'openid.ns': 'http://specs.openid.net/auth/2.0',
-        'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
-        'openid.claimed_id':
-          'http://specs.openid.net/auth/2.0/identifier_select',
-        'openid.return_to': returnTo,
-        'openid.realm': realm
-      }
+        "openid.mode": "checkid_setup",
+        "openid.ns": "http://specs.openid.net/auth/2.0",
+        "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
+        "openid.claimed_id":
+          "http://specs.openid.net/auth/2.0/identifier_select",
+        "openid.return_to": returnTo,
+        "openid.realm": realm,
+      },
     },
     token: {
       async request() {
         // May throw an error, dunno should I handle it or no
-        // prettier-ignore
-        const claimedIdentifier = await verifyAssertion(req.url!, realm, returnTo)
+        const claimedIdentifier = await verifyAssertion(
+          req.url!,
+          realm,
+          returnTo
+        );
 
         if (!claimedIdentifier) {
-          throw new Error('Unauthenticated')
+          throw new Error("Unauthenticated");
         }
 
         const matches = claimedIdentifier.match(
           /^https?:\/\/steamcommunity\.com\/openid\/id\/(\d+)$/
-        )
+        );
 
         if (!matches) {
-          throw new Error('Unauthenticated')
+          throw new Error("Unauthenticated");
         }
 
         return {
           tokens: new TokenSet({
-            id_token: randomUUID(),
+            id_token: matches[1],
             access_token: randomUUID(),
-            steamId: matches[1]
-          })
-        }
-      }
+          }),
+        };
+      },
     },
     userinfo: {
       async request(ctx) {
         const response = await fetch(
-          `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${ctx.provider.clientSecret}&steamids=${ctx.tokens.steamId}`
-        )
+          `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${ctx.provider.clientSecret}&steamids=${ctx.tokens.id_token}`
+        );
 
-        const data = await response.json()
-
-        return data.response.players[0]
-      }
+        const data = await response.json();
+        return data.response.players[0];
+      },
     },
     profile(profile: SteamProfile) {
-      // next.js can't serialize the session if email is missing or null, so I specify user ID
+      /*
+       NextJS requires that the user object returned from the callback has an email property.
+       So we generate a fake email address using the user's Steam ID.
+      */
       return {
         id: profile.steamid,
         image: profile.avatarfull,
-        email: `${profile.steamid}@${EMAIL_DOMAIN}`,
-        name: profile.personaname
-      }
-    }
-  }
+        email: `${profile.steamid}@steamcommunity.com`,
+        name: profile.personaname,
+      };
+    },
+    options: rest,
+  };
 }
 
 /**
@@ -118,24 +144,24 @@ async function verifyAssertion(
   realm: string,
   returnTo: string
 ): Promise<string | null> {
-  const party = new RelyingParty(returnTo, realm, true, false, [])
+  const party = new RelyingParty(returnTo, realm, true, false, []);
 
   const result: {
-    authenticated: boolean
-    claimedIdentifier?: string | undefined
+    authenticated: boolean;
+    claimedIdentifier?: string | undefined;
   } = await new Promise((resolve, reject) => {
     party.verifyAssertion(url, (error, result) => {
       if (error) {
-        reject(error)
+        reject(error);
       } else {
-        resolve(result!)
+        resolve(result!);
       }
-    })
-  })
+    });
+  });
 
   if (!result.authenticated) {
-    return null
+    return null;
   }
 
-  return result.claimedIdentifier!
+  return result.claimedIdentifier!;
 }
